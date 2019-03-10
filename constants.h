958b77868c3ebb6b9755806d75c39d00e46b1ade
YuriiAndrukhiv <yurii.andrukhiv@gmail.com>
2019-03-10 13:16:39 +0200

    Separated constans in individual file, created type alias Maze and updated program

---
+++ constants.h
@@ -0,0 +1,7 @@
+const int cMazeColumns = 12;
+const int cMazeRows = 8;
+
+const char cEmptySymbol = ' ';
+const char cWallSymbol = 'X';
+const char cCharacterSymbol = '@';
+const char cExitSymbol = '#';
