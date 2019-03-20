c402452b63db6f9d800d56e2f05b22f34bc23b8b
YuriiAndrukhiv <yurii.andrukhiv@gmail.com>
2019-03-21 00:44:29 +0200

    Added random generation labyrinth

--- constants.h
+++ constants.h
@@ -1,7 +1,8 @@
-const int cMazeColumns = 12;
-const int cMazeRows = 8;
+const int cMazeColumns = 31;
+const int cMazeRows = 9;
 
 const char cEmptySymbol = ' ';
 const char cWallSymbol = 'X';
 const char cCharacterSymbol = '@';
 const char cExitSymbol = '#';
+const char cKeySymbol = 'Þ';
--- maze-game.cpp
+++ maze-game.cpp
@@ -6,6 +6,48 @@
 
 using Maze = std::array<std::array<char, cMazeColumns>, cMazeRows>;
 
+void mazify(Maze &prMaze)
+{
+	for (int row = 0; row < cMazeRows; row++)
+    {
+        for (int column = 0; column < cMazeColumns; column++)
+        {
+             if(row %2 == 0 || column%2 == 0)
+                prMaze[row][column] = cWallSymbol;
+            else
+                prMaze[row][column] = cEmptySymbol;
+        }
+    }
+    for (int row = 1; row < cMazeRows - 1; row++) // generating 2 free lines
+    {
+        for (int column = 1; column < cMazeColumns -1; column++)
+        {
+            prMaze[1][column]= cEmptySymbol;
+            prMaze[row][cMazeColumns-2]= cEmptySymbol;
+        }
+    }
+    for (int row = 3; row < cMazeRows - 1; row+=2)
+    {
+        for (int column = 1; column < cMazeColumns - 3; column+=2)
+        {
+            static	std::random_device rd;   // non-deterministic generator
+            static	std::mt19937 gen(rd());  // to seed mersenne twister.
+            static	std::uniform_int_distribution<> dist(0, 1);
+
+            int dir = dist(gen);
+
+            if(dir == 0)
+            {
+                prMaze[row-1][column] = cEmptySymbol;
+            }
+            if(dir == 1)
+            {
+                prMaze[row][column + 1] = cEmptySymbol;
+            }
+        }
+    }
+}
+
 // Checks if user want so play
 // Return true if user wants to play; false otherwise
 bool doesUserWantsToPlay()
@@ -39,20 +81,7 @@ bool doesUserWantsToPlay()
 //              (walls added, added character and exit)
 void generateMaze(Maze &prMaze)
 {
-    for (int row = 0; row < cMazeRows; row++)
-    {
-        for (int column = 0; column < cMazeColumns; column++)
-        {
-            if ((row == 0) || (row == cMazeRows - 1) || (column == 0) || (column == cMazeColumns - 1))
-            {
-                prMaze[row][column] = cWallSymbol;
-            }
-            else
-            {
-                prMaze[row][column] = cEmptySymbol;
-            }
-        }
-    }
+    mazify(prMaze);
 
     // Place character - always at positon 1,1
     prMaze[1][1] = cCharacterSymbol;
@@ -60,9 +89,20 @@ void generateMaze(Maze &prMaze)
     // Place exit randomly
     static std::random_device rd;
     static std::mt19937 mt{rd()};
-    static std::uniform_int_distribution<int> rowGenerator = std::uniform_int_distribution<int>(2, cMazeRows - 2);
-    static std::uniform_int_distribution<int> columnGenerator = std::uniform_int_distribution<int>(2, cMazeColumns - 2);
-    prMaze[rowGenerator(mt)][columnGenerator(mt)] = cExitSymbol;
+    static std::uniform_int_distribution<int>    rowExitGenerator;
+    static std::uniform_int_distribution<int> columnExitGenerator;
+    do
+    {
+      rowExitGenerator = std::uniform_int_distribution<int>(2, cMazeRows - 2);
+      columnExitGenerator = std::uniform_int_distribution<int>(2, cMazeColumns - 2);
+    }
+     while(prMaze[rowExitGenerator(mt)][columnExitGenerator(mt)] != cEmptySymbol);
+     prMaze[rowExitGenerator(mt)][columnExitGenerator(mt)] = cExitSymbol;
+
+// Place key randomly
+    static std::uniform_int_distribution<int> rowKeyGenerator = std::uniform_int_distribution<int>(2, cMazeRows - 2);
+    static std::uniform_int_distribution<int> columnKeyGenerator = std::uniform_int_distribution<int>(2, cMazeColumns - 2);
+    prMaze[rowKeyGenerator(mt)][columnKeyGenerator(mt)] = cKeySymbol;
 }
 
 // Draws maze onto screen
@@ -129,8 +169,8 @@ char moveAndGather(int row,
     if (command == "l")
     {
         rCharMovedOnto = prMaze[row][column - 1];
-
-        if (rCharMovedOnto != cWallSymbol)
+ 
+        if (rCharMovedOnto != cWallSymbol && rCharMovedOnto != cExitSymbol)
         {
             column--;
         }
@@ -140,7 +180,7 @@ char moveAndGather(int row,
     {
         rCharMovedOnto = prMaze[row][column + 1];
 
-        if (rCharMovedOnto != cWallSymbol)
+        if (rCharMovedOnto != cWallSymbol && rCharMovedOnto != cExitSymbol)
         {
             column++;
         }
@@ -150,7 +190,7 @@ char moveAndGather(int row,
     {
         rCharMovedOnto = prMaze[row - 1][column];
 
-        if (rCharMovedOnto != cWallSymbol)
+        if (rCharMovedOnto != cWallSymbol && rCharMovedOnto != cExitSymbol)
         {
             row--;
         }
@@ -160,7 +200,7 @@ char moveAndGather(int row,
     {
         rCharMovedOnto = prMaze[row + 1][column];
 
-        if (rCharMovedOnto != cWallSymbol)
+        if (rCharMovedOnto != cWallSymbol&& rCharMovedOnto != cExitSymbol)
         {
             row++;
         }
@@ -180,6 +220,21 @@ void gameMessage(const std::string& message)
     std::cout << std::endl << message << std::endl << std::endl;
 }
 
+//The function cheks if the key in the field
+bool checkIfExistscKeySymbol(Maze maze)
+{
+    for (int row = 0; row < cMazeRows; row++)
+    {
+      for (int column = 0; column < cMazeColumns; column++)
+      {
+		if (maze[row][column] == cKeySymbol)
+           {
+            	return true;
+           }
+      }
+    }
+	return false;
+}
 // Moves character and check if exit was found as a result of that move.
 // Parameters:
 //      prMaze - reference to maze field; will be modified while player moves.
@@ -199,8 +254,12 @@ bool moveCharacterAndCheckIfExitFound(Maze &prMaze)
         {
             gameMessage("Cannot move here!");
         }
-
         if (charMovedOnto == cExitSymbol)
+	  {
+            gameMessage("Firstly take the key!");
+        }
+
+        if (charMovedOnto == cExitSymbol && !checkIfExistscKeySymbol(prMaze))
         {
             gameMessage("Exit found!");
 		 return true;
@@ -231,6 +290,7 @@ void playMazeGame()
 
 int main()
 {
+setlocale(LC_CTYPE, "ukr");
     while (doesUserWantsToPlay())
     {
         playMazeGame();
